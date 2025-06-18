f1ed598ffb7b02f001632077b2401e67959a96fc
lssnath <nl1447430@gmail.com>
2025-06-18 11:48:59 -0400

    sim

---
+++ src/routes/dicionario/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { dicionario } from '$lib/dicionario';
+
+    let palavra = ''; 
+    let filtradas = dicionario; 
+
+    function buscar() {
+        if (palavra === '') {
+            filtradas = dicionario; 
+            return;
+        }
+
+        filtradas = dicionario.filter(termo =>
+  termo.palavra.toLowerCase().includes(palavra.toLowerCase())
+);
+
+    }
+</script>
+
+<div>
+    <input 
+        placeholder="Digite uma palavra" 
+        oninput={buscar} 
+        bind:value={palavra} 
+    />
+</div>
+
+<div>
+    <ol>
+        {#each filtradas as termo}
+            <li>
+                <a href="/dicionario/{termo.palavra}">{termo.palavra}</a>
+            </li>
+        {/each}
+    </ol>
+</div>
---
+++ src/routes/dicionario/[palavra]/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { page } from '$app/stores';
+    import { dicionario } from '$lib/dicionario';
+  
+    const palavra = $page.params.palavra.toLowerCase();
+    const termo = dicionario.find(item => item.palavra.toLowerCase() === palavra);
+  </script>
+  
+  
+  {#if termo}
+    <div>
+      <h1>{termo.palavra}</h1>
+      <ul>
+        {#each termo.definicoes as definicao}
+          <li>{definicao}</li>
+        {/each}
+      </ul>
+      <a href="/dicionario">Voltar</a>
+    </div>
+  {:else}
+    <div>
+      <p>Palavra não encontrada!</p>
+      <a href="/dicionario">Voltar</a>
+    </div>
+  {/if}
+  
